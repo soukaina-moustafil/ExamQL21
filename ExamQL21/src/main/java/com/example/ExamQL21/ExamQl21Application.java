d0a28f31974b6cbf1743eaaba2d96179e435c4b4
soukaina <moustafilsoukaina@gmail.com>
2021-12-21 14:53:32 +0100

    init project

---
+++ ExamQL21/src/main/java/com/example/ExamQL21/ExamQl21Application.java
@@ -0,0 +1,13 @@
+package com.example.ExamQL21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQl21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQl21Application.class, args);
+	}
+
+}
---
+++ ExamQL21/src/test/java/com/example/ExamQL21/ExamQl21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamQL21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQl21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
